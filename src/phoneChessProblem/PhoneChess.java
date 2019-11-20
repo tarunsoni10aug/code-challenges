06df9aacd737e05dba2581ceceab9056a54681b1
tarunsoni10aug <tarunsoni10aug@gmail.com>
2019-11-20 17:24:30 +0530

    changes in file

--- src/phoneChessProblem/PhoneChess.java
+++ src/phoneChessProblem/PhoneChess.java
@@ -11,9 +11,9 @@ public class PhoneChess {
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in); 
-        System.out.println("please Enter start number between 0-9");
+        System.out.println("please Enter start number between 0-9 \n");
 		String startNumber = sc.nextLine();
-		System.out.println("please Enter chess Piece [queen, knight, king,rook, bishop,pawn]");
+		System.out.println("please Enter chess Piece [queen, knight, king,rook, bishop,pawn] \n");
 		String pieceName=sc.nextLine();
 		PhoneChessUtils.checkValidChessPiece(pieceName);
 		PhoneChessUtils.checkValidStartNumber(startNumber);
